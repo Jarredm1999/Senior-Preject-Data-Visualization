8fb715383addadfa509f2bd93f101c45ba0bc4e7
Jarred Myers <jarredm1999@gmail.com>
2021-02-17 19:47:22 -0600

    First attempt at a basic chart, using data from a dictionary

--- index.js
+++ index.js
@@ -6,10 +6,34 @@ document.addEventListener('DOMContentLoaded', function(e) {
         { id: 'd4', value: 6, region: 'Germany'},
     ];
 
-    d3.select('div')
+    /*d3.select('div')
       .selectAll('p')
       .data(DUMMY_DATA)
       .enter()
       .append('p')
-      .text(dta => dta.region);
+      .text(dta => dta.region);*/
+
+      const xScale = d3.scaleBand()
+        .domain(DUMMY_DATA.map((dataPoint) => dataPoint.region))
+        .rangeRound([0, 250])
+        .padding(0.1);
+
+      const yScale = d3.scaleLinear()
+        .domain([0, 15])
+        .range([200, 0]);
+
+
+      const container = d3.select('svg')
+        .classed('container', true);//uses an css element
+
+      const bars = container
+      .selectAll('.bar')
+      .data(DUMMY_DATA)
+      .enter()
+      .append('rect')
+      .classed('bar', true)
+      .attr('width', xScale.bandwidth())
+      .attr('height', data => 200 - yScale(data.value))
+      .attr('x', data => xScale(data.region))
+      .attr('y', data => yScale(data.value));
 });
