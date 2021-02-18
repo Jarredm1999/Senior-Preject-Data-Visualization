08e5b26ceec022f4ebf9474dbd7e0bf2070c4b85
Jarred Myers <jarredm1999@gmail.com>
2021-02-17 22:46:13 -0600

    The code is now reading data from a json file and using that data do create the chart.

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 document.addEventListener('DOMContentLoaded', function(e) {
-    const DUMMY_DATA = [
-        { id: 'd1', value: 10, region: 'USA'},
-        { id: 'd2', value: 11, region: 'India'},
-        { id: 'd3', value: 12, region: 'China'},
-        { id: 'd4', value: 6, region: 'Germany'},
-    ];
+     
+    let TEST_DATA;
 
-    /*d3.select('div')
-      .selectAll('p')
-      .data(DUMMY_DATA)
-      .enter()
-      .append('p')
-      .text(dta => dta.region);*/
+    fetch("test.json")
+    .then(response => response.json())
+    .then(data => { 
+        console.log(data.data)
+        TEST_DATA = JSON.parse(JSON.stringify(data.data));
+        console.log(TEST_DATA);
 
-      const xScale = d3.scaleBand()
-        .domain(DUMMY_DATA.map((dataPoint) => dataPoint.region))
-        .rangeRound([0, 250])
-        .padding(0.1);
+        const xScale = d3.scaleBand()
+            .domain(TEST_DATA.map((dataPoint) => dataPoint.region))
+            .rangeRound([0, 250])
+            .padding(0.1);
 
-      const yScale = d3.scaleLinear()
-        .domain([0, 15])
-        .range([200, 0]);
+        const yScale = d3.scaleLinear()
+            .domain([0, 15])
+            .range([200, 0]);
 
 
-      const container = d3.select('svg')
-        .classed('container', true);//uses an css element
+        const container = d3.select('svg')
+            .classed('container', true);//uses an css element
 
-      const bars = container
-      .selectAll('.bar')
-      .data(DUMMY_DATA)
-      .enter()
-      .append('rect')
-      .classed('bar', true)
-      .attr('width', xScale.bandwidth())
-      .attr('height', data => 200 - yScale(data.value))
-      .attr('x', data => xScale(data.region))
-      .attr('y', data => yScale(data.value));
+        const bars = container
+        .selectAll('.bar')
+        .data(TEST_DATA)
+        .enter()
+        .append('rect')
+        .classed('bar', true)
+        .attr('width', xScale.bandwidth())
+        .attr('height', data => 200 - yScale(data.value))
+        .attr('x', data => xScale(data.region))
+        .attr('y', data => yScale(data.value));
+    });
 });
