858952435ce3cd62ee63c6e8dcac8350ef0aa4e0
Jarred Myers <jarredm1999@gmail.com>
2021-03-03 15:25:46 -0600

    Updated comments

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-//If you are trying to run this code you need to run a python server. 
+//If you are trying to run this code you need to run a python server.
+//The server command is "python -m http.server".
 //If you do not you will get an error saying "Fetch API cannot load...URL
 //scheme must be http or https for CORS request".
 
 document.addEventListener('DOMContentLoaded', function(e) {
-     
+//Storing the different types of data from the json into variables. 
     let TEST_DATA;
     let NEXT_DATA;
     let LINE_DATA;
-
+//reading the json file.
     fetch("test.json")
     .then(response => response.json())
     .then(data => {
@@ -24,6 +25,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
         LINE_DATA = JSON.parse(JSON.stringify(data.linedata));
         console.log(LINE_DATA);
 
+//--------------------This is the code for both bar charts----------------------------------
     const MARGINS = {top: 20, bottom: 10};
     const CHART_WIDTH = 600;
     const CHART_HEIGHT = 400 - MARGINS.top - MARGINS.bottom;
@@ -31,21 +33,22 @@ document.addEventListener('DOMContentLoaded', function(e) {
     const x = d3.scaleBand().rangeRound([0, CHART_WIDTH]).padding(0.1);
     const y = d3.scaleLinear().range([CHART_HEIGHT, 0]);
 
+//Selects the svg element in the html file and sets the width and height.
     const chartContainer = d3.select('svg')
         .attr('width', CHART_WIDTH)
         .attr('height', (CHART_HEIGHT + MARGINS.top + MARGINS.bottom));
-
+//Setting the values of the x and y axis to values from our dataset.
     x.domain(TEST_DATA.map((d) => d.region));
     y.domain([0, d3.max(TEST_DATA, d => d.value) + 3]);
 
 
     const chart = chartContainer.append('g');
-
+//sets the x axis and translates it to correct position.
     chart.append('g')
          .call(d3.axisBottom(x).tickSizeOuter(0))
          .attr('transform', `translate(0, ${CHART_HEIGHT})`)
          .attr('color', '#000000');
-
+//
         chart.selectAll('.bar')
          .data(TEST_DATA, data => data.id)
          .enter()
@@ -55,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
          .attr('height', data => CHART_HEIGHT - y(data.value))
          .attr('x', (data) => x(data.region))
          .attr('y', (data) => y(data.value));
-
+//Adding the value above each bar.
         chart.selectAll('.label')
             .data(TEST_DATA, data => data.id)
             .enter()
@@ -65,26 +68,26 @@ document.addEventListener('DOMContentLoaded', function(e) {
             .attr('y', data => y(data.value) - 20)
             .attr('text-anchor', 'middle')
             .classed('label', true);
-
+//Creating new x and y constants since we are creating another chart.
     const x2 = d3.scaleBand().rangeRound([0, CHART_WIDTH]).padding(0.1);
     const y2 = d3.scaleLinear().range([CHART_HEIGHT, 0]);
-
+//Selecting the html element with the id "chart2" and setting the width and height attribute.
     const chartContainer2 = d3.select('#chart2')
         .append('svg')
         .attr('width', CHART_WIDTH)
         .attr('height', (CHART_HEIGHT + MARGINS.top + MARGINS.bottom));
-
+//Setting the domain of the x and y constants to the values from our dataset.
     x2.domain(NEXT_DATA.map((d) => d.region));
     y2.domain([0, d3.max(NEXT_DATA, d => d.value) + 3]);
 
 
     const chart2 = chartContainer2.append('g');
-
+//Creating the axis and translating it to the correct position.
     chart2.append('g')
          .call(d3.axisBottom(x2).tickSizeOuter(0))
          .attr('transform', `translate(0, ${CHART_HEIGHT})`)
          .attr('color', '#000000');
-
+//Select the html element with the bar class and assigns the values to the coorespoding bar.
         chart2.selectAll('.bar')
          .data(NEXT_DATA, data => data.id)
          .enter()
@@ -94,7 +97,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
          .attr('height', data => CHART_HEIGHT - y2(data.value))
          .attr('x', (data) => x2(data.region))
          .attr('y', (data) => y2(data.value));
-
+//Adding the value above each bar.
         chart2.selectAll('.label')
             .data(NEXT_DATA, data => data.id)
             .enter()
@@ -104,22 +107,25 @@ document.addEventListener('DOMContentLoaded', function(e) {
             .attr('y', data => y2(data.value) - 20)
             .attr('text-anchor', 'middle')
             .classed('label', true);
-        
-    
+//------------------This is the code for the pie chart--------------------------------        
+//Sets the random color for each peice of the pie
     let colors = d3.scaleOrdinal(d3.schemeDark2);
+//Selects the html element with the "pie" id and assigns the height, width and background
     let svg = d3.select('#pie')
                 .append('svg')
                 .attr('width', 600)
                 .attr('height', 500)
                 .style('background', 'grey');
-    
+//Parses the data into the values for each peice of the pie    
     let pieData = d3.pie().sort(null).value(function(d){return d.value})(TEST_DATA);
     console.log(pieData);
+//General sizing for each segment 
     let segments = d3.arc()
                      .innerRadius(0)
                      .outerRadius(200)
                      .padAngle(.05)
                      .padRadius(50);
+//Creates the sections and adds the correct color for the cooresponding value
     let sections = svg.append('g')
                      .attr('transform', 'translate(225, 250)')
                      .selectAll('path').data(pieData);
@@ -128,7 +134,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
             .attr('fill', function(d) {
                 return colors(d.value);
             });
-
+//Sets up the legend with the correct color, label and value            
     let legends = svg.append('g').attr('transform', 'translate(450, 100)')
                     .selectAll('legends').data(pieData);
     let legend = legends.enter().append('g').classed('legends', true)
@@ -143,62 +149,62 @@ document.addEventListener('DOMContentLoaded', function(e) {
           .attr('fill', 'black')
           .attr('x', 35)
           .attr('y', 15);
-    
+//-----------------------This is the code for the line graph--------------------------------
+//Setup new margin values to accommodate for the increased size
     let margin = {top: 20, right: 40, bottom: 30, left: 50},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
-
+//Parses the time data
     let parseTime = d3.timeParse('%Y');
-
+//Setup new x and y variables for the new chart
     let x3 = d3.scaleTime().range([0, width]);
     let y3 = d3.scaleLinear().range([height, 0]);
-
+//Assign the valueline variable with the date and value
     let valueline = d3.line()
             .x(function(d) { return x3(d.date); })
             .y(function(d) { return y3(d.value); });
-
+//Selects the html element with "lnchrt" as the id and sets the height, width
+//and translates it to the correct position.
     let svg2 = d3.select('#lnchrt').append('svg')
             .attr('width', width + margin.left + margin.right)
             .attr('height', height + margin.top + margin.bottom + 25)
             .append('g')
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    let label = d3.select('.label');
-
+//Function to draw the graph
     function draw(data, option) {
         data = data[option];
-
+//Parse Json data
         data.forEach(function(d) {
             d.date = parseTime(d.date);
             d.value = +d.value;
         });
-
+//Sorts the data incase the years are out of order
         data.sort(function(a, b) {
             return a["date"]-b["date"];
         });
-
+//Sets the domain of the x and y variable the largest date and value
         x3.domain(d3.extent(data, function(d) {return d.date; }));
         y3.domain([0, d3.max(data, function(d) {
             return Math.max(d.value)
         })]);
-
+//Setup the path the line will go on
         svg2.append('path')
             .data([data])
             .attr('class', 'line')
             .attr('d', valueline);
-
+//Setup the x axis
         svg2.append('g')
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x3));
-
+//Text label for the x axis
         svg2.append("text")             
             .attr("transform", "translate(" + (width/2) + " ," + (height + margin.top + 20) + ")")
             .style("text-anchor", "middle")
             .text("Year");
-        
+//Sets up the y axis
         svg2.append('g')
             .call(d3.axisLeft(y3));
-        
+//Text label for the y axis and positions it in the correct spot        
         svg2.append("text")
             .attr("transform", "rotate(-90)")
             .attr("y", 0 - margin.left)
@@ -206,8 +212,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
             .attr("dy", "1em")
             .style("text-anchor", "middle")
             .text("Value");      
-
-        
+//Plots a circle at each point
         svg2.selectAll("circle")
 		.data(data)
 		.enter()
@@ -220,7 +225,8 @@ document.addEventListener('DOMContentLoaded', function(e) {
 	        return y3(d.value)
 	    });
     }
-
+//Json request and calls the draw function. This enables us to be able to draw more then
+//line on the line graph.
     d3.json('test.json').then(function(data) {
         draw(data, "linedata");
     })
